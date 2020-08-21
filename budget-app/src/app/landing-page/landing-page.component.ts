31bb39f8e7d50fc4ea891b3065a2da7c0ed1e390
Makafui Naaman <makafuinaaman@Makafuis-MacBook-Pro.local>
2020-08-21 08:59:49 -0500

    started working on the landing-page component

--- budget-app/src/app/landing-page/landing-page.component.ts
+++ budget-app/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalBudget } from 'src/shared/budget-item.model'
 
 @Component({
   selector: 'app-landing-page',
@@ -8,6 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class LandingPageComponent implements OnInit {
 
   constructor() { }
+    title = "Budget Calculator"
+    items: ModalBudget[] = new Array<ModalBudget>()
+    totalBudget: number = 0
+
+    addItem(eventNewItem: ModalBudget){
+      this.items.push(eventNewItem)
+      this.totalBudget += eventNewItem.amount
+    }
+
+    deleteItem(eventItem: ModalBudget){
+      let index = this.items.indexOf(eventItem)
+      this.items.splice(index,1)
+      this.totalBudget -= eventItem.amount
+    }
+
+   
 
   ngOnInit(): void {
   }
