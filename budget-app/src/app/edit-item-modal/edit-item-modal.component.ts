9eb9c2dd34238913bc5bcecea601a4e857786a57
Makafui Naaman <makafuinaaman@Makafuis-MacBook-Pro.local>
2020-08-21 08:58:50 -0500

    Started working on the edit-item modal componenet

--- budget-app/src/app/edit-item-modal/edit-item-modal.component.ts
+++ budget-app/src/app/edit-item-modal/edit-item-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { ModalBudget } from 'src/shared/budget-item.model'
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'  
 
 @Component({
   selector: 'app-edit-item-modal',
@@ -7,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditItemModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<EditItemModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public item: ModalBudget) { }
 
+onSubmitted(updatedItem: ModalBudget){
+  this.dialogRef.close(updatedItem)
+}
   ngOnInit(): void {
   }
 
