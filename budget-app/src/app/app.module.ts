42e39afbb6824a60c09975260e748bdfeaa7404f
Makafui Naaman <makafuinaaman@Makafuis-MacBook-Pro.local>
2020-08-21 08:56:03 -0500

    implemented fix to the routing and module

--- budget-app/src/app/app.module.ts
+++ budget-app/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireAuthGuard} from '@angular/fire/auth-guard'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { EditItemModalComponent } from './edit-item-modal/edit-item-modal.component';
 import { AddChangesModalComponent } from './add-changes-modal/add-changes-modal.component';
 import { EditBudgetItemListComponent } from './edit-budget-item-list/edit-budget-item-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BudgetItemCardComponent } from './edit-budget-item-list/budget-item-card/budget-item-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations' 
+import { CustomMaterialModule } from '../materialUI-design/index'
 
 @NgModule({
   declarations: [
@@ -14,13 +18,19 @@ import { EditBudgetItemListComponent } from './edit-budget-item-list/edit-budget
     LandingPageComponent,
     EditItemModalComponent,
     AddChangesModalComponent,
-    EditBudgetItemListComponent
+    EditBudgetItemListComponent,
+    BudgetItemCardComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule
   ],
-  providers: [],
+  providers: [AngularFireAuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
